327759c0eff2a6b9d8f17f083b27414d0a852e96
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-10-22 19:56:54 +0900

    Initial commit

---
+++ src/com/koreait/first/Print.java
@@ -0,0 +1,28 @@
+package com.koreait.first;
+
+public class Print {
+    public static void main(String[] args) {
+
+    }
+
+
+    /*
+        클래스명 : 파스칼 케이스 기법
+        나머지 : 카멜 케이스 기법
+
+        (단어들) my, name, is, heum
+        파스칼 : MyNameIsHuem
+        카멜 : myNameIsHeum
+     */
+
+    // single-line comment
+    /* multi-line comment */
+
+    /*
+    주석입니다.
+    sdflksadjhfklsdf
+
+
+    asdkjfhskajfhksajfklsjadflkj
+     */
+}
